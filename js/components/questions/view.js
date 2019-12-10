a86b80ae1b7417ce3e708bf6630eb4df51466a7c
Pikilon <pikilon@gmail.com>
2019-12-10 15:37:00 +0100

    Styled with bulma

--- js/components/questions/view.js
+++ js/components/questions/view.js
@@ -2,42 +2,46 @@ import {TAG as vStyle} from '../v-style.js'
 export const selectors = {
   question: 'question',
   question__color: 'question__color',
-  question__answers: 'question__answers',
+  question__answer: 'question__answer',
   question__result: 'question__result',
+  question__checkbox: 'question__checkbox',
 }
 const s = selectors;
+const cssVars = {
+  colorSize: '3rem',
+}
 export const style = //css
 `
-  .${s.question} {
-    display: flex;
-    font-size: 1rem;
-  }
-  .${s.question} + .${s.question} {
-    margin-top: 1em;
+  .${s.question__result} {
+    font-size: ${cssVars.colorSize};
   }
   .${s.question__color} {
-    width: 1em;
-    height: 1em;
-  }
-  .${s.question__answers} {
-    position: relative;
-    padding-right: 2em;
+    display: block;
+    width: ${cssVars.colorSize};
+    height: ${cssVars.colorSize};
   }
 `
 
 export const template = // html
 `
-<ul>
+<section>
   <${vStyle}>${style}</${vStyle}>
-  <li class="${s.question}" v-for="(question, index) in questions">
-    <span class="${s.question__color}" v-bind:style="{backgroundColor: question.color}"></span>
-    <div class="${s.question__answers}">
-      <label v-for="answer in answers">
-        <input type="radio" v-model="questions[index].answer" :value="answer" class="${s.question__checkbox}">
-        {{answer}}
-      </label>
-      <span v-if="questions[index].answer" className="${s.question__result}">{{questions[index].answer === questions[index].right ? '✅' : '❌' }}</span>
-    </div>
-  </li>
-</ul>
+  <article class="${s.question} box" v-for="(question, index) in questions">
+    <div class="media">
+      <div class="media-left">
+        <span class="${s.question__color}" v-bind:style="{backgroundColor: question.color}"></span>
+      </div>
+      <div class="media-center">
+        <label v-for="answer in answers" class="${s.question__answer} is-block">
+          <input type="radio" v-model="questions[index].answer" :value="answer" class="${s.question__checkbox}">
+          {{answer}}
+        </label>
+      </div>
+      <div class="media-right ${s.question__result}" v-if="questions[index].answer">
+        {{questions[index].answer === questions[index].right ? '✅' : '❌' }}
+      </div>
+  </div>
+
+  </article>
+</section>
 `
