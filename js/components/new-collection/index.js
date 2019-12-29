1c3df585f2dbb33fe2668defd08bfb23a4089157
Pikilon <pikilon@gmail.com>
2019-12-29 11:47:46 +0100

    Create new test started

--- js/components/new-collection/index.js
+++ js/components/new-collection/index.js
@@ -1,40 +1,23 @@
 import { template } from './view.js'
 
+const TITLE_MIN_LENGTH = 4
+
 export default Vue.extend({
   template,
   data: () => ({
     valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 4) || 'Name must be less than 4 characters',
+    title: '',
+    titleRules: [
+      v => !!v || 'Title is required',
+      v => (v && v.length >= TITLE_MIN_LENGTH) || `Name must at leat than ${TITLE_MIN_LENGTH} characters`,
     ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-    ],
-    select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-    ],
-    checkbox: false,
   }),
 
     methods: {
       validate () {
         if (this.$refs.form.validate()) {
-          this.snackbar = true
+          console.log('this.title', this.title);
         }
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
       },
     },
 })
--- js/components/new-collection/view.js
+++ js/components/new-collection/view.js
@@ -1,25 +1,11 @@
 export const template = /*html*/
 `
 <v-form ref="form" v-model="valid" lazy-validation>
-  <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required/>
+  <v-text-field v-model="title" :rules="titleRules" label="Test title" required/>
 
-  <v-text-field v-model="email" :rules="emailRules" label="E-mail" required/>
-
-  <v-select v-model="select" :items="items" :rules="[v => !!v || 'Item is required']" label="Item" required></v-select>
-
-  <v-checkbox v-model="checkbox" :rules="[v => !!v || 'You must agree to continue!']" label="Do you agree?" required>
-  </v-checkbox>
-
-  <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
-    Validate
+  <v-btn :disabled="!valid || !title" color="success" class="mr-4" @click="validate">
+    Create
   </v-btn>
 
-  <v-btn color="error" class="mr-4" @click="reset">
-    Reset Form
-  </v-btn>
-
-  <v-btn color="warning" @click="resetValidation">
-    Reset Validation
-  </v-btn>
 </v-form>
 `
